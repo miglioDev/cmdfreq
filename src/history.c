f3ca427e69aa85a3a1154a724eeb87c0438468ea
miglioDev <204082785+miglioDev@users.noreply.github.com>
2025-12-20 18:56:52 +0100

    Add history_read_file function
    Implement function to read file and handle errors.

--- src/history.c
+++ src/history.c
@@ -1,1 +1,23 @@
-//first commit
+//history.c
+
+
+int history_read_file(const char *path)
+{
+	FILE *fp;
+	char buffer[512];
+	
+	fp = fopen(path, "r"); 
+	if(fp == NULL) return -1;
+	else 
+  {
+	  while(fgets(buffer, 512, fp) != NULL)
+	  {
+	     //add substring rsrc 
+	  }
+  }
+	
+	
+	fclose(fp);
+	
+	return 0;
+}
