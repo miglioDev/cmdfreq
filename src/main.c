4e8e41f6cb4ca314c4cce54eda8869f73a006cb0
miglioDev <204082785+miglioDev@users.noreply.github.com>
2026-02-17 17:14:07 +0100

    Implement argument check

--- src/main.c
+++ src/main.c
@@ -2,8 +2,13 @@
 #include <stdlib.h>
 #include "history.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])  //now on set up and planning
 {
+    if (argc != 2) {
+    fprintf(stderr, "Usage: %s <history_file>\n", argv[0]);
+    return EXIT_FAILURE; }
+
+    //CmdHistory 
     const char *history_file = get_default_history();
 
     CmdStats *stats = load_history(history_file);
