63d5a3a5c321297201d041a53c76ee373b402a15
miglioDev <204082785+miglioDev@users.noreply.github.com>
2026-02-20 15:13:39 +0100

    Add planning for h.c

--- src/history.c
+++ src/history.c
@@ -1,23 +1,10 @@
 //history.c
 
+/*
+note: add fun and transfer code
 
-int history_read_file(const char *path)
-{
-	FILE *fp;
-	char buffer[512];
-	
-	fp = fopen(path, "r"); 
-	if(fp == NULL) return -1;
-	else 
-  {
-	  while(fgets(buffer, 512, fp) != NULL)
-	  {
-	     //add substring rsrc 
-	  }
-  }
-	
-	
-	fclose(fp);
-	
-	return 0;
-}
+extract_base_command
+find_command
+sort_entries 
+
+*/
